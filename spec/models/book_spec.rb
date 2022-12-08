a5810dc6978c2ad5aa64cda5301d44e126869dc3
Deannah Burke <deannah@bierstadtlager.com>
2022-12-08 09:56:58 -0700

    Created db, schema, first model test

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :model do
+  describe 'validations' do
+     it {should validate_presence_of :title}
+     it {should validate_presence_of :author}
+     it {should validate_presence_of :summary}
+     it {should validate_presence_of :publication_year}
+     it {should validate_presence_of :genre}
+
+  end
+end
