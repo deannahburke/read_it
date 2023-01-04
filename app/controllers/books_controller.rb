38af51a22aab478c913f787dc526099bd3107aae
Deannah Burke <deannah@bierstadtlager.com>
2023-01-04 10:58:08 -0700

    Add sad path for error message, padding for form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,20 @@ class BooksController < ApplicationController
     def new
 
     end
+
+    def create
+        book = Book.new(book_params)
+        if book.save
+            redirect_to "/books"
+            flash[:message] = "Book added!"
+        else 
+            flash[:alert] = "Please fill out all fields."
+            redirect_to "/books/new"
+        end
+    end
+
+    private
+        def book_params
+            params.permit(:title, :author, :publication_year, :genre, :summary)
+        end
 end 
