298806fcafa9407e1673086271e1f2498df7ebea
Deannah Burke <deannah@bierstadtlager.com>
2023-01-31 11:18:51 -0700

    Add sad path to destroy spec, fix table header format

--- spec/features/destroy_spec.rb
+++ spec/features/destroy_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe 'destroy a book', type: :feature do 
     it 'can delete a book' do
         certain_hunger = Book.create!(title: "A Certain Hunger", publication_year: 2020, author: "Chelsea G. Summers", genre: "Horror", summary: "A food critic turns cannibal")
-        
+        roadside_picnic = Book.create!(title: "Roadside Picnic", publication_year: 1972, author: "Arkady Strugatsky", genre: "Sci-Fi", summary: "Alien invasion of small town in Russia")
+
         visit "/books"
 
         within "#book-#{certain_hunger.id}" do 
@@ -13,5 +14,6 @@ RSpec.describe 'destroy a book', type: :feature do
 
         expect(current_path).to eq("/books")
         expect(page).to_not have_content("A Certain Hunger")
+        expect(page).to have_content("Roadside Picnic")
     end
 end
