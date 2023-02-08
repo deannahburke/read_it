b5b5d5c37f19cbb1267f6d57bd2d1c1312fd0623
Deannah Burke <deannah@bierstadtlager.com>
2023-02-08 09:45:53 -0700

    Change file structure to add books and wishlist folders, create wishlist index spec file

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   post '/books', to: 'books#create'
   patch '/books/:book_id', to: 'books#update'
   delete '/books/:book_id', to: 'books#destroy'
+  
   get '/wishlist', to: 'wishlist#index'
 end
